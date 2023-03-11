31abf8d32270143ae35ecfe088163c99c4224b05
Hossein Alizadeh <40279197+Alizadeh128@users.noreply.github.com>
2023-03-11 14:33:02 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,5 @@
 <?php
-    $books= [
-        [
-            "title" => "The first book",
-            "Author" => "Author1",
-            "publish-date" => "1395",
-            "price" => 100000
-        ],
-        [
-            "title" => "The new first book",
-            "Author" => "Author1",
-            "publish_date" => "1395",
-            "price" => 100000
-        ],
-        [
-            "title" => "The second book",
-            "Author" => "The second Author",
-            "publish-date" => "1399",
-            "price" => 200000
-        ],
-        [
 
-            "title" => "The third book",
-            "Author" => "The third Author",
-            "publish-date" => "1401",
-            "price" => 350000
-        ]
-    ];
     function filter($books,$fn)
     {
         $filteredbook = [];
